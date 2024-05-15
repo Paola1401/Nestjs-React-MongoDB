66c8ebef32ca7b7e12436c2499663db9bbca2790
Paola1401 <162546569+Paola1401@users.noreply.github.com>
2024-05-15 10:45:30 -0600

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const HomePage = () => {
-    return (
-      <div>HomePage</div>
-    )
-}
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>BiciAmigo</h1>
+      <p>Bienvenido a BiciAmigo, ayudaremos con tu proxima carrera.</p>
+      <img
+        src="/assets/portada.jpg" // Ruta relativa desde la raíz del servidor
+        alt="Portada de BiciAmigo" // Texto descriptivo
+        style={{ maxWidth: '100%', height: 'auto' }}
+      /> 
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
